cda0ace3b993b9f77e7871ec1f08b8f287fdf21b
Theodor Sjöstedt <theodor.sjostedt@gmail.com>
2012-09-02 16:27:51 +0200

    Added Week class, modified Day class

--- classes/Day.js
+++ classes/Day.js
@@ -6,11 +6,11 @@ function Day(name) {}
 Day.prototype.name = name; // For example 'Monday'
 Day.prototype.tasks = new Array(); // holds day instances
 
-Day.prototype.add = function(task){
+Day.prototype.addTask = function(task){
 	this.tasks.push(task);
 }
 
-Day.prototype.remove = function(task){
+Day.prototype.removeTask = function(task){
 	var id = this.tasks.indexOf(task);
 	if(id < 0) {
 		console.debug('Task does not exist in tasklist');
---
+++ classes/Week.js
@@ -0,0 +1,25 @@
+/*
+ * Week class
+ */
+function Week(number) {
+	this.number = number;
+	this.hours = 0.0;
+	_days = new Array();
+	
+	this.addDay = function(day) {
+		_days.push(day);
+	}
+	
+	this.removeDay = function(day) {
+		var id = _days.indexOf(day);
+		if(id < 0) {
+			console.debug('Day does not exist in daylist');
+			return;
+		}
+		_days.splice(id,1);
+	}
+}
+
+function() {
+	var w = new Week();
+}
